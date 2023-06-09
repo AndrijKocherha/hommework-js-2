16a8d038000a89d03feda358b0ca12e4da3f1c91
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2023-06-09 17:24:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+"use-strict"
+//? task 0
+   //let a = 10 // Змінна а
+   //let b = 15 // Змінна b
+   //let c = 5 // Змінна c
+   //let sumOne = a + 12 + b //рахуєм суму 1
+   //console.log(sumOne) // виводим  суму 1
+   //let sumTwo = Math.sqrt((a + b) / (2 * a)) // рахуєм суму 2
+   //console.log(parseFloat(sumTwo.toFixed(3))) // виводим  суму 2
+   //let sumThree = Math.cbrt((a + b) * c) // рахуєм суму 3
+   //console.log(sumThree) // виводим  суму 3
+//=====================================================================
+//? task 1
+   //let a = 10 // Змінна а
+   //let b = 15 // Змінна b
+   //let c = 5 // Змінна c
+   //let sumOne = a + 12 + b //рахуєм суму 1
+   //console.log(sumOne) // виводим  суму 1
+   //let sumTwo = Math.sqrt((a + b) / (2 * a)) // рахуєм суму 2
+   //console.log(parseFloat(sumTwo.toFixed(3))) // виводим  суму 2
+   //let sumThree = Math.cbrt((a + b) * c) // рахуєм суму 3
+   //console.log(sumThree) // виводим  суму 3
+//============================================================
+//? task 2
+//let presentYear = 2023; // тепрішній рік
+//const BIRHTDAY = parseInt(prompt("ваш рік народження")); // ваш рік народження
+//let yourAge = presentYear - BIRHTDAY; // рахує  стільки вам років
+//document.write(`ваш рік народження ${yourAge}`)
+//===================================================
+//? task 3
+//let productPrice = parseFloat(prompt("ціна товару")) // вказуєм ціну
+//let countProduct = parseInt(prompt("кількість продукту")) // кількіть продуктів
+//let productsPrice = productPrice * countProduct // рахуєм загальну вартість 
+//let productsVAT = (5 * productsPrice) / 100
+//document.write(`
+//   Загальна ціна ${productsPrice} <br/>
+//   ПДВ ${productsVAT}
+//`)
+
+//===================================================
+//? task 4
+//let centimetersLenght = parseFloat(prompt("введіть сантиметри"))
+//let metersLenght = centimetersLenght / 100// переводим сантиметрив метри 
+//let kilometersLenght = centimetersLenght / 100000 // переводим сантиметрив кілометри
+//document.write(`
+//   ${metersLenght} метрів <br/>
+//   ${kilometersLenght} кілометрів
+//`)
+
+//===================================================
+//? task 5
+//const SECONDSIN_IN_ERA = 86400
+//let seconsds = parseFloat(prompt("кількість секунд з початку доби" ))
+//let minutes = (SECONDSIN_IN_ERA - seconsds) / 60
+//let hours = (SECONDSIN_IN_ERA - seconsds) / 3660
+//document.write(`
+//   хвилин ${minutes} <br/>
+//   годин ${hours}
+//`)
+//? task 6
+//let productQuantity = parseInt(prompt("кількість товарів"))
+//let productPrice = parseFloat(prompt("ціна товару"))
+//let totalPrice  = productPrice * productQuantity
+//document.write(`
+//   товар: ${productPrice} (${productQuantity}X) <br/>
+//   ==============================================
+//   загальна ціна: ${totalPrice}
+//`)
+
+//? task 6
+//let randomMounth = Math.floor(Math.random() * (12 - 1) + 1);
+
